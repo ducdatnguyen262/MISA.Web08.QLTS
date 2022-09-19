667998f65935690b510b2de5d87dbeda076d651e
ducdatnguyen262 <ducdatnguyen262@gmail.com>
2022-09-19 14:07:23 +0700

    update Asset

--- MISA.Web08.QLTS.API/Entities/Asset.cs
+++ MISA.Web08.QLTS.API/Entities/Asset.cs
@@ -27,5 +27,71 @@ namespace MISA.Web08.QLTS.API.Entities
         /// </summary>
         public Gender Gender { get; set; }
 
+        /// <summary>
+        /// ID phòng ban
+        /// </summary>
+        public Guid DepartmentId { get; set; }
+
+        /// <summary>
+        /// ID loại tài sản
+        /// </summary>
+        public Guid CategoryId { get; set; }
+
+        /// <summary>
+        /// Ngày mua
+        /// </summary>
+        public DateTime PurchaseDate { get; set; }
+
+        /// <summary>
+        /// Giá tiền
+        /// </summary>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// Số lượng
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Tỉ lệ hao mòn (%)
+        /// </summary>
+        public double DepreciationRate { get; set; }
+
+        /// <summary>
+        /// Năm theo dõi
+        /// </summary>
+        public int TrackedYear { get; set; }
+
+        /// <summary>
+        /// Số năm sử dụng
+        /// </summary>
+        public int LifeTime { get; set; }
+
+        /// <summary>
+        /// Năm sử dụng
+        /// </summary>
+        public DateTime ProductionYear { get; set; }
+
+        public Boolean Active { get; set; }
+
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Người tạo
+        /// </summary>
+        public string? CreatedBy { get; set; }
+
+        /// <summary>
+        /// Ngày sửa gần nhất
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Người sửa gần nhất
+        /// </summary>
+        public string? ModifiedBy { get; set; }
     }
 }
